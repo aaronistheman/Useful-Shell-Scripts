749f385160115fe68c39f58d92b6fac70a9ac9a9
aaronistheman <aarons.7007@gmail.com>
2015-09-02 14:41:34 -0700

    Changed the order of the parameters in change-release-number.sh

--- change-release-number.sh
+++ change-release-number.sh
@@ -3,10 +3,10 @@
 # @pre release number in specified file is placed after the text
 # "Release number: "
 # @post release number has been changed in specified file
-# @param $1 the file to edit
-# @param $2 the old release number
-# @param $3 the new release number
-# @example ./change-release-number.sh play.php 1.1 2.0
+# @param $1 the old release number
+# @param $2 the new release number
+# @param $3 the file to edit
+# @example ./change-release-number.sh 1.1 2.0 play.php
 
 # Make the edit
-sed -i "s~Release number: $2~Release number: $3~" $1
+sed -i "s~Release number: $1~Release number: $2~" $3
